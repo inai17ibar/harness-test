5fb60aa1c9690aa6420f70cd667985572edc7092
inai17ibar's Mac mini <inai17ibar@inai17ibarsnoMac-mini.local>
2026-06-22 11:06:53 +0900

    fix: resolve issue #17 - min関数を実装して

--- src/min.js
+++ src/min.js
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 export function min() {}
+=======
+export function min(arr) {
+  if (arr.length === 0) return undefined;
+  return Math.min(...arr);
+}
+>>>>>>> 7386323 (fix: resolve issue #17 - min関数を実装して)
